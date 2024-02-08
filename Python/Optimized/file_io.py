d99dd9f6c4a433ec57ee1a02169fe40383a8f108
Jan-Luca Hubald <jl.hubald@hotmail.de>
2024-02-08 16:53:11 +0100

    Finish first version of optimized model

---
+++ Python/Optimized/file_io.py
@@ -0,0 +1,172 @@
+import os
+import csv
+import time
+import numpy as np
+
+
+def read_input_file(file_path=os.path.abspath(os.getcwd() + '/input/input_parameters.txt')):
+    """
+    Reads input parameters from an input file and returns them as a dictionary.
+
+    Args:
+        file_path (str): Directory of the input file. Defaults to /input/input_parameters.txt.
+
+    Returns:
+        dict: Input parameters and their respective keywords.
+    """
+    input_parameters = {}
+
+    with open(file_path, 'r') as file:
+        for line in file:
+            line = line.strip()
+            if line:
+                key, value = line.split('=')
+                value = value.strip()
+
+                # Check if the value is a list
+                if value.startswith('[') and value.endswith(']'):
+                    value = value[1:-1]  # Remove the square brackets
+                    # Split the values by comma and strip leading/trailing spaces
+                    value = [v.strip() for v in value.split(',')]
+
+                    # Convert individual elements of the list to int or float if possible
+                    converted_values = []
+                    for v in value:
+                        try:
+                            if '.' in v:
+                                converted_values.append(float(v))
+                            else:
+                                converted_values.append(int(v))
+                        except ValueError:
+                            converted_values.append(v)  # Keep the value as a string
+
+                    value = converted_values
+
+                else:
+                    # Convert the value to the appropriate data type
+                    try:
+                        if '.' in value:
+                            value = float(value)  # Convert to float if value contains a dot
+                        else:
+                            value = int(value)  # Convert to integer if value is an integer string
+                    except ValueError:
+                        if value.startswith('"') and value.endswith('"') or value.startswith("'") and value.endswith("'"):
+                            value = value[1:-1]  # Remove quotation marks if present
+
+                input_parameters[key.strip()] = value
+
+    return input_parameters
+
+
+def save_arrays(arrays, outputDir):
+    arrayNames = ["satParameters", "satConstants", "probabilityMatrix"]
+    for arr in arrays:
+        if len(arr.shape) < 2:
+            arrayNames = ["satParameters", "satConstants", "distances"]
+    for ii, arr in enumerate(arrays):
+        fileName = arrayNames[ii] + ".csv"
+        fileDir = os.path.join(outputDir, fileName)
+        np.savetxt(fileDir, arr, delimiter=',')
+
+
+def read_arrays(arrayDir):
+    try:
+        # Load the CSV file into a numpy array
+        data = np.genfromtxt(arrayDir, delimiter=',')
+
+        return data
+
+    except IOError as e:
+        print("Error reading CSV file:", str(e))
+        return None
+
+
+def create_header(timestamps, input_parameters):
+    """
+    Creates a list containing lines to be printed into the output .csv file header.
+
+    Args:
+        filename (str): Name of the used data file.
+        timestamps (list): List of starting time, ending time and elapsed time.
+        input_parameters (dict): Input parameters of the neural network.
+
+    Returns:
+        tuple: A tuple containing a list and a string:
+            - header (list): A list of lines to be printed into the output file header
+            - sample_nzmber (str): Sample number.
+
+    """
+    header = []
+    header_line = "Hubald model simulation data"
+    header.append(header_line)
+    header.append("")
+    line = "starting_time = " + str(timestamps[0])
+    header.append(line)
+    line = "finishing_time = " + str(timestamps[1])
+    header.append(line)
+    line = "elapsed_time = " + str(timestamps[2]) + " s"
+    header.append(line)
+    for key in input_parameters:
+        line = key + " = " + str(input_parameters[key])
+        header.append(line)
+    return header
+
+
+def write_results_to_csv(simulationData, fileHeader, output_directory=os.path.abspath(os.getcwd() + '/output')):
+    """
+    Write the results into csv file.
+
+    Args:
+
+
+    Returns:
+        None
+    """
+    currentTime = time.strftime("%a %b %d %H:%M:%S %Y")  # Get the current time in the desired format
+    currentTime = currentTime.replace(" ", "_")  # Replace spaces with underscores
+    currentTime = currentTime[4:]
+    currentTime = currentTime.replace(":", "-")  # Replace colons with hyphens or any other desired character
+    outputFileNameame = "hubald_simulation.csv"
+    filepath = os.path.join(output_directory, outputFileNameame)
+    transposedData = np.transpose(simulationData)
+
+    # Create the last folder if the directory below exists
+    lastFolder = os.path.dirname(filepath)
+    if not os.path.exists(lastFolder):
+        os.makedirs(lastFolder)
+
+    file_mode = 'w' if os.path.isfile(filepath) else 'x'
+    with open(filepath, file_mode, newline='') as csvfile:
+        writer = csv.writer(csvfile, delimiter=';')
+        for line in fileHeader:
+            writer.writerow([line])
+        writer.writerow([])
+        writer.writerow(["Time", "Collisions per iteration", "Total collisions", "Total satellites",
+                         "Active satellites", "Passive satellites", "Small fragments", "Large fragments",
+                         "Small fragment collisions", "Large fragment collisions"])
+        writer.writerows(transposedData)
+
+
+def update_input_file(new_values, file_path=os.path.abspath(os.getcwd() + '/Input/input_parameters.txt')):
+    """
+    Updates the values in an input file based on the provided dictionary.
+
+    Args:
+        new_values (dict): Dictionary containing the values to be written into the input file.
+        file_path (str): Directory of the input file.
+
+    Returns:
+        None
+    """
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+
+    with open(file_path, 'w') as file:
+        for line in lines:
+            if line.strip():
+                key, value = line.split('=')
+                key = key.strip()
+                if key in new_values:
+                    updated_value = new_values[key]
+                    line = f"{key}={updated_value}\n"
+            file.write(line)
--- Python/Optimized/test.py
+++ Python/Optimized/test.py
@@ -1,7 +1,9 @@
+import time
 from calc import initialize, calculate_trig
-from prob_matrix import build_prob_matrix, update_prob_matrix
+from prob_matrix import build_prob_matrix, update_prob_matrix, parallel_update
+from dynamics import small_fragment, large_fragment, satellite_collision, deorbits, starts
 
-num_sats = int(1000)
+num_sats = int(10)
 earth_radius = float(6_370_000)
 a_low = float(earth_radius + 200_000)
 a_high = float(earth_radius + 2_000_000)
@@ -14,7 +16,7 @@ accuracy = int(20)
 sin = calculate_trig(accuracy, "s")
 cos = calculate_trig(accuracy, "c")
 
-sigma = float(2000)
+sigma = float(5000000)
 time_step = int(3)
 prob_thresh = float(10 ** (-15))
 lower_bound =int(0)
@@ -22,9 +24,48 @@ upper_bound = int(sat_parameters.shape[0])
 col_prob_matrix = build_prob_matrix(sat_parameters, sat_constants, sigma, time_step, accuracy, prob_thresh, sin, cos,
                                     lower_bound, upper_bound)
 
-launched_sats = int(10)
+frag_col_prob = 4
+num_small_fragments = 100000000
+mm = frag_col_prob / 12 / num_small_fragments * time_step
+small_fragment_args = (col_prob_matrix, sat_parameters, sat_constants, num_small_fragments, mm,
+                       time_step, sigma, accuracy, prob_thresh, sin, cos)
 
-col_prob_matrix = update_prob_matrix(sat_parameters, sat_constants, sigma, time_step, accuracy, prob_thresh, sin, cos,
-                                     col_prob_matrix, launched_sats, a_low, a_high, active_fraction, plane)
+# print(sat_parameters[:, 6])
+# start = time.time()
+# col_prob_matrix, sat_parameters, satellites_struck = small_fragment(*small_fragment_args)
+# finish = time.time()
+# print(sat_parameters[:, 6])
+# print(f"Matrix updated after {finish - start}s")
 
-print(col_prob_matrix)
+frag_col_prob = 4
+num_large_fragments = 1000000
+mm = frag_col_prob / 12 / num_large_fragments * time_step
+large_fragment_args = (col_prob_matrix, sat_parameters, sat_constants, num_small_fragments, num_large_fragments, mm)
+
+# print(sat_parameters)
+# col_prob_matrix, sat_parameters, sat_constants, _, _ = large_fragment(*large_fragment_args)
+# print(sat_parameters)
+
+# print(col_prob_matrix)
+# sat_collision_args = (col_prob_matrix, sat_parameters, sat_constants, num_small_fragments, num_large_fragments)
+# col_prob_matrix, sat_parameters, sat_constants, fragments, num_collisions = satellite_collision(*sat_collision_args)
+# print(col_prob_matrix)
+
+deorbits_per_timestep = int(10)
+deorbit_args = (col_prob_matrix, sat_parameters, sat_constants, deorbits_per_timestep)
+
+# print(sat_parameters[:, 6])
+# col_prob_matrix, sat_parameters, sat_constants = deorbits(*deorbit_args)
+# print(sat_parameters[:, 6])
+
+mode = "single"
+num_workers = 8
+launched_sats = int(5)
+
+launch = {"mode": mode, "num_workers": num_workers, "sigma": sigma, "time_step": time_step, "accuracy": accuracy,
+          "prob_thresh": prob_thresh, "sin": sin, "cos": cos, "starts_per_timestep": launched_sats, "a_low": a_low,
+          "a_high": a_high, "active_fraction": active_fraction, "plane": plane}
+
+print(sat_parameters.shape[0])
+col_prob_matrix, sat_parameters, sat_constants = starts(col_prob_matrix, sat_parameters, sat_constants, launch)
+print(sat_parameters.shape[0])
